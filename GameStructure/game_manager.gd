93145cf7f0fa7ee8d80d08619932b2bc54d96a76
Orangechrisy <christinakclementine@gmail.com>
2026-07-22 16:38:28 -0700

    event random chance
    chance for events to appear for provinces

--- GameStructure/game_manager.gd
+++ GameStructure/game_manager.gd
@@ -18,6 +18,7 @@ func end_day() -> void:
 	GameState.update_day()
 	# Anything else we want to do
 	reduce_days_to_revolution()
+	roll_events()
 
 # reduce_days_to_revolution: Calculates number of days to lose, then updates
 # Variables: NONE (for now)
@@ -26,3 +27,18 @@ func reduce_days_to_revolution() -> void:
 	var days_to_reduce: int = 1
 	# Run some calculations here based on events that happened
 	GameState.reduce_days_to_revolution(days_to_reduce)
+
+# how many events do we want to have happen?
+# from most to least likely (fervor?) roll event odds for each province 
+# if not enough events, roll again
+func roll_events() -> void:
+	var owned_provinces: Array[Node2D] = GameState.provinces.filter(func(province): return province.curr_owner == 0)
+	owned_provinces.sort_custom(func(a, b): return a.fervor > b.fervor)
+	var num_events: int = max(owned_provinces.size(), randi() % 4)
+	while num_events > 0:
+		for i in owned_provinces:
+			if num_events > 0:
+				if owned_provinces[i].roll_event_odds():
+					num_events -= 1
+	
+	
--- map provinces/province.gd
+++ map provinces/province.gd
@@ -1,10 +1,11 @@
+@tool
 extends Node2D
 
 @export var vertices: PackedVector2Array
 @export var province_name: String
 @export var province_image: Sprite2D
-@export var current_owner: int # not sure what this should be, but could be an enum
-@export var potential_events: Array
+@export var curr_owner: int = 0 # not sure what this should be, but could be an enum
+@export var potential_events: Array[Resource]
 @export_group("Counters")
 @export var base_food: float
 @export var base_gold: float
@@ -36,14 +37,26 @@ func _on_day_update(new_day):
 	base_food = base_food + food_yield - food_consumption
 	base_gold = base_gold + gold_yield - gold_consumption
 	if fervor > loyalty:
-		current_owner = 1
-	# change image? update potential events?
+		curr_owner = 1
+	# change image?
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+# try to do the event, based on rng and variables of the province or something
+func try_event(event: MapEvent) -> bool:
+	# TODO: determine event odds somehow
+	return true
 
+# gets the possible events, shuffles them, and tries to fire them, if one fires it returns true
+func roll_event_odds() -> bool:
+	var possible_events: Array[Resource] = potential_events.filter(func(event): return event.can_appear())
+	possible_events.shuffle()
+	for event in possible_events:
+		if try_event(event):
+			return true
+	return false
 
+# the province has been clicked on
 func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 		print("province ", province_name, " clicked")
+
+# TODO: province hover over (to see stats?)
