f685a1d2626ad9774e3f6a1a6af531aa60b2c1dc
nopecopter <nopecopter@gmail.com>
2026-07-22 13:21:18 -0600

    - Added more signals to GameState. - Started setting up for days to rebellion counter.

--- GameStructure/game_manager.gd
+++ GameStructure/game_manager.gd
@@ -11,6 +11,18 @@ func _process(delta: float) -> void:
 
 # Custom functions
 
+# end_day: Runs all end-of-day functions, like variable updates and rolling new events
+# Variables: NONE (for now)
+# Returns: void
 func end_day() -> void:
 	GameState.update_day()
 	# Anything else we want to do
+	reduce_days_to_revolution()
+
+# reduce_days_to_revolution: Calculates number of days to lose, then updates
+# Variables: NONE (for now)
+# Returns: void
+func reduce_days_to_revolution() -> void:
+	var days_to_reduce: int = 1
+	# Run some calculations here based on events that happened
+	GameState.reduce_days_to_revolution(days_to_reduce)
