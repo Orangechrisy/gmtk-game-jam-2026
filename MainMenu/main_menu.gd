bd04ab0563f39865ac21bd77c1b3a25020133c34
Orangechrisy <christinakclementine@gmail.com>
2026-07-24 07:20:12 -0700

    menu back up signals
    sub menus like options send back signals when back button hit, so menus holding it can become visible

--- MainMenu/main_menu.gd
+++ MainMenu/main_menu.gd
@@ -1,27 +1,29 @@
-extends Node2D
+extends Control
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
+	for sub_menu in get_children():
+		if sub_menu.has_signal("back_up"):
+			sub_menu.connect("back_up", on_back_up)
 
 func _on_start_game_button_pressed() -> void:
 	pass # Replace with function body.
 
 
 func _on_options_button_pressed() -> void:
+	$MainMainMenu.visible = false
 	$OptionsMenu.visible = true
 
 
 func _on_credits_button_pressed() -> void:
+	$MainMainMenu.visible = false
 	$CreditsMenu.visible = true
 
 
 func _on_quit_game_button_pressed() -> void:
+	$MainMainMenu.visible = false
 	$QuitConfirmMenu.visible = true
+
+func on_back_up() -> void:
+	$MainMainMenu.visible = true
--- MainMenu/options_menu.gd
+++ MainMenu/options_menu.gd
@@ -1,15 +1,7 @@
 extends Control
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
+signal back_up()
 
 func _on_back_button_pressed() -> void:
 	self.visible = false # TODO: Impvoe
+	back_up.emit()
