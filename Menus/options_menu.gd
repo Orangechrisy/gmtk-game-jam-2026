f750b4ed5549f189aba93e7479d9601b900efcde
SheerAweso <northcuttjake@rocketmail.com>
2026-07-26 01:40:48 -0500

    options done

--- Menus/options_menu.gd
+++ Menus/options_menu.gd
@@ -1,4 +1,4 @@
-extends Control
+extends CanvasLayer
 
 signal back_up()
 
@@ -12,3 +12,18 @@ func play_click():
 
 func _on_mouse_entered() -> void:
 	MusicManager.play_sfx(MusicManager.SFX.MOUSEOVER)
+
+func _on_mainvolume_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_linear(AudioServer.get_bus_index("Master"), value)
+
+func _on_musicvolume_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_linear(AudioServer.get_bus_index("Music"), value)
+
+func _on_sfxvolume_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_linear(AudioServer.get_bus_index("SFX"), value)
+
+func _on_fullscreen_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
