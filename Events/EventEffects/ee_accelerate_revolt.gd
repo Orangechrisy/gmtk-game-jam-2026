f82e7352d16164b598453945553a2ca5f6b1d069
SheerAweso <northcuttjake@rocketmail.com>
2026-07-26 11:24:27 -0500

    small tweaks

--- Events/EventEffects/ee_accelerate_revolt.gd
+++ Events/EventEffects/ee_accelerate_revolt.gd
@@ -4,6 +4,7 @@ class_name EEAccelerateRevolt
 func do_effect() -> void:
 	GameState.revolt_accelerated = true
 	GameManager.add_to_results_popup("Revolution Accelerated")
+	GameState.days_to_revolution_updated.emit(GameState.days_to_revolution-1)
 
 func get_effect_desc() -> String:
 	return "Accelerate Revolution by 1 Day"
